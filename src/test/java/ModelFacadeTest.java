ff3a8c88ca16af5d713ac29fa83f6115e252c9b0
Baocheng Wang <bwan3675@uni.sydney.edu.au>
2021-05-26 19:58:25 +0800

    TDD ROUND-1 : GREEN - Integration Testing for credentials validation, email sending, get words info from cache, check if word is in the cache, and update the cache

--- src/test/java/ModelFacadeTest.java
+++ src/test/java/ModelFacadeTest.java
@@ -1,11 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
 
-import java.controller.HTTPManager;
+import controller.HTTPManager;
 import model.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
 import static org.mockito.AdditionalMatchers.gt;
 import static org.mockito.Mockito.*;
 
@@ -42,8 +46,30 @@ public class ModelFacadeTest {
                 "text/plain", "some sort of email","bc");
     }
 
+    @Test
+    public void verifyGetWordCheckCache(){
+        model.getWord("word");
+        verify(mockDB, atLeast(1)).entityExists("word");
+    }
 
+    @Test
+    public void verifyGetWordGetFromCache(){
+        model.getWord("word");
+        when(mockDB.entityExists("word")).thenReturn(null);
+        verify(mockHTTP, atLeast(1)).getWord("id","key", "entries","en-us","word");
+    }
 
+    @Test
+    public void verifyUpdateCache(){
+        JSONParser parser = new JSONParser();
+        try{
+            JSONObject info = (JSONObject) parser.parse("{\"a\":\"info\"}");
+            model.updateDB("a", info);
+            verify(mockDB, atLeast(1)).updateEntity("a", info);
+        } catch (ParseException e){
+            fail();
+        }
+    }
 
 
 }
